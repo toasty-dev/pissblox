ba38f02777329640b78b404a8c2aefb7425e1ec6
toasty.dev <120754355+toasty-dev@users.noreply.github.com>
2024-05-31 05:26:02 -0500

    Create solaris_bootstrapper.lua

---
+++ solaris_bootstrapper.lua
@@ -0,0 +1,19 @@
+--[[
+  _________      .__               .__        
+ /   _____/ ____ |  | _____ _______|__| ______
+ \_____  \ /  _ \|  | \__  \\_  __ \  |/  ___/
+ /        (  <_> )  |__/ __ \|  | \/  |\___ \ 
+/_______  /\____/|____(____  /__|  |__/____  >
+        \/                 \/              \/ 
+made with love by toasty.
+]]
+
+local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Consistt/Ui/main/UnLeaked"))()
+
+if game.PlaceId == 286090429 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/toasty-dev/pissblox/main/solariscb_beta.lua",true))()
+elseif game.PlaceId == 301549746 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/toasty-dev/pissblox/main/solarisarsenal_beta.lua",true))()
+else
+    local notsupported = lib:InitNotifications():Notify("This game isn't supported for Solaris.", 3, "information")
+end
