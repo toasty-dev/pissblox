b34d4222402bde675069a3dc095c946c96f18c9f
toasty.dev <120754355+toasty-dev@users.noreply.github.com>
2024-07-04 01:26:49 -0500

    Update solaris_bootstrapper.lua

--- solaris_bootstrapper.lua
+++ solaris_bootstrapper.lua
@@ -16,6 +16,8 @@ elseif game.PlaceId == 301549746 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/toasty-dev/pissblox/main/solariscb_beta.lua",true))()
 elseif game.PlaceId == 1480424328 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/toasty-dev/pissblox/main/solariscb_beta.lua",true))()
+elseif game.PlaceId == 1869597719 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/toasty-dev/pissblox/main/solariscb_beta.lua",true))()
 else
-    local notsupported = lib:InitNotifications():Notify("This game isn't supported for Solaris.", 3, "information")
+    local notsupported = lib:InitNotifications():Notify("This game isn't supported for Solaris yet. If this game is popular please add in suggestions", 3, "information")
 end
