85f98678ef298308e57711de186e972b7768352f
toasty.dev <120754355+toasty-dev@users.noreply.github.com>
2024-07-22 10:38:27 -0500

    Update SaveManager.lua

--- UI/addons/SaveManager.lua
+++ UI/addons/SaveManager.lua
@@ -201,7 +201,7 @@ local SaveManager = {} do
 	function SaveManager:BuildConfigSection(tab)
 		assert(self.Library, 'Must set SaveManager.Library')
 
-		local section = tab:AddRightGroupbox('Configuration')
+		local section = tab:AddLeftGroupbox('Configuration')
 
 		section:AddDropdown('SaveManager_ConfigList', { Text = 'Config list', Values = self:RefreshConfigList(), AllowNull = true })
 		section:AddInput('SaveManager_ConfigName',    { Text = 'Config name' })
