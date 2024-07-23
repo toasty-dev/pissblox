a873f561c34ef23403f04e14564c189a9b00e025
toasty.dev <120754355+toasty-dev@users.noreply.github.com>
2024-07-23 18:47:40 -0500

    Update moddedlinora.lua

--- UI/moddedlinora.lua
+++ UI/moddedlinora.lua
@@ -3444,7 +3444,7 @@ function Library:CreateWindow(...)
         local State = InputService.MouseIconEnabled;
 
         local Cursor = Drawing.new('Triangle');
-        Cursor.Thickness = 5;
+        Cursor.Thickness = 2;
         Cursor.Filled = true;
 
         while Outer.Visible and ScreenGui.Parent do
