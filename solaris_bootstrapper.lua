191a8763e4011d27fb6fe3cef8135584ae55547f
toasty.dev <120754355+toasty-dev@users.noreply.github.com>
2024-08-05 09:51:37 -0500

    Update solaris_bootstrapper.lua

--- solaris_bootstrapper.lua
+++ solaris_bootstrapper.lua
@@ -18,7 +18,17 @@ elseif game.PlaceId == 1480424328 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/toasty-dev/pissblox/main/games/solariscb.lua",true))()
 elseif game.PlaceId == 1869597719 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/toasty-dev/pissblox/main/games/solariscb.lua",true))()
-elseif game.PlaceId == 13772394625 then
+elseif game.PlaceId == 16281300371 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/toasty-dev/pissblox/main/games/solarisbb.lua",true))()
+elseif game.PlaceId == 14915220621 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/toasty-dev/pissblox/main/games/solarisbb.lua",true))()
+elseif game.PlaceId == 15144787112 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/toasty-dev/pissblox/main/games/solarisbb.lua",true))()
+elseif game.PlaceId == 16581648071 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/toasty-dev/pissblox/main/games/solarisbb.lua",true))()
+elseif game.PlaceId == 15517169103 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/toasty-dev/pissblox/main/games/solarisbb.lua",true))()
+elseif game.PlaceId == 15131065025 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/toasty-dev/pissblox/main/games/solarisbb.lua",true))()
 else
     local notsupported = lib:InitNotifications():Notify("This game isn't supported for Solaris yet. If this game is popular please add in suggestions", 3, "information")
